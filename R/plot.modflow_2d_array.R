91a8783bc3bde68af81904c7b694e42be612acdb
rogiersbart <brogiers@sckcen.be>
2015-08-23 12:22:36 +0200

    roxygen2 version change + compatibility with RTOOLZ dev version

--- R/plot.modflow_2d_array.R
+++ R/plot.modflow_2d_array.R
@@ -18,7 +18,7 @@
 #' @param prj projection file object
 #' @param target_CRS coordinate reference system for the plot
 #' @param alpha transparency value; defaults to 1
-#' @param plot3D logical; should a 3D plot be made
+#' @param plot3d logical; should a 3D plot be made
 #' @param height 2D array for specifying the 3D plot z coordinate
 #' @return ggplot2 object or layer; if plot3D is TRUE, nothing is returned and the plot is made directly
 #' @method plot modflow_2d_array
@@ -26,26 +26,23 @@
 #' @import ggplot2 directlabels akima rgl RTOOLZ
 plot.modflow_2d_array <- function(modflow_2d_array, dis, ba6=NULL, mask=ifelse0(is.null(ba6),modflow_2d_array*0+1,ba6$IBOUND[,,1]), colour_palette=rev_rainbow, zlim = range(modflow_2d_array, finite=TRUE), levels = pretty(zlim, nlevels), nlevels = 7, type='fill', add=FALSE,height_exaggeration=100,binwidth=round(diff(zlim)/20),label=TRUE,prj=NULL,target_CRS=NULL,alpha=1,plot3d=FALSE,height=NULL)
 {
-  if(plot3d)
-  {
+  if(plot3d) {
     x <- (cumsum(dis$DELR)-dis$DELR/2)
     y <- sum(dis$DELC) - (cumsum(dis$DELC)-dis$DELC/2)
     z <- t(height)*height_exaggeration
-    if(!add) open3d()
+    if(!add) rgl::open3d()
     colorlut <- colorRampPalette(colour_palette(nlevels))(25) # height color lookup table
     col <- colorlut[ round(approx(seq(zlim[1],zlim[2],length=25+1),seq(0.5,25+0.5,length=25+1),xout=c(t(modflow_2d_array)),rule=2)$y) ] # assign colors to heights for each point
     alpha <- rep(1,length(col))
     alpha[which(c(t(mask))==0)] <- 0
-    if(type=='fill') surface3d(x,y,z,color=col,alpha=alpha,back='lines',smooth=FALSE) 
-    if(type=='grid') surface3d(x,y,z,front='lines',alpha=alpha,back='lines',smooth=FALSE) 
-  } else
-  {
+    if(type=='fill') rgl::surface3d(x,y,z,color=col,alpha=alpha,back='lines',smooth=FALSE) 
+    if(type=='grid') rgl::surface3d(x,y,z,front='lines',alpha=alpha,back='lines',smooth=FALSE) 
+  } else {
     xy <- expand.grid(cumsum(dis$DELR)-dis$DELR/2,sum(dis$DELC)-(cumsum(dis$DELC)-dis$DELC/2))
     names(xy) <- c('x','y')
     mask[which(mask==0)] <- NA
     
-    if(type=='fill')
-    {  
+    if(type=='fill') {  
       ids <- factor(1:(dis$NROW*dis$NCOL))
       xWidth <- rep(dis$DELR,dis$NROW)
       yWidth <- rep(dis$DELC,each=dis$NCOL)
@@ -59,22 +56,17 @@ plot.modflow_2d_array <- function(modflow_2d_array, dis, ba6=NULL, mask=ifelse0(
       positions$y[(seq(3,nrow(positions),4))] <- positions$y[(seq(3,nrow(positions),4))] + yWidth/2
       positions$y[(seq(4,nrow(positions),4))] <- positions$y[(seq(4,nrow(positions),4))] - yWidth/2
       values <- data.frame(id = ids,value = c(t(modflow_2d_array*mask^2)))
-      if(!is.null(prj))
-      {
+      if(!is.null(prj)) {
         new_positions <- convert_dis_to_real(x=positions$x,y=positions$y,prj=prj)
         positions$x <- new_positions$x
         positions$y <- new_positions$y
       }
-      if(!is.null(target_CRS))
-      {
-        new_positions <- addosmmerc(data.frame(x=positions$x,y=positions$y),CRS_from=CRS(prj$projection),CRS_to=target_CRS)
-        positions$x <- new_positions$Xosmmerc
-        positions$y <- new_positions$Yosmmerc                    
+      if(!is.null(target_CRS)) {
+        positions <- convert_coordinates(positions,from=CRS(prj$projection),to=target_CRS)
       }
       datapoly <- merge(values, positions, by=c("id"))
       datapoly <- na.omit(datapoly)
-      if(add)
-      {
+      if(add) {
         return(geom_polygon(aes(x=x,y=y,fill=value, group=id),data=datapoly,alpha=alpha))# +
           #scale_fill_gradientn(colours=colour_palette(nlevels),limits=zlim)) # solve this issue!
       } else {
@@ -83,9 +75,7 @@ plot.modflow_2d_array <- function(modflow_2d_array, dis, ba6=NULL, mask=ifelse0(
           scale_fill_gradientn(colours=colour_palette(nlevels),limits=zlim) +
           coord_equal())
       }
-    }
-    if(type=='grid')
-    {  
+    } else if(type=='grid') {  
       ids <- factor(1:(dis$NROW*dis$NCOL))
       xWidth <- rep(dis$DELR,dis$NROW)
       yWidth <- rep(dis$DELC,each=dis$NCOL)
@@ -99,22 +89,17 @@ plot.modflow_2d_array <- function(modflow_2d_array, dis, ba6=NULL, mask=ifelse0(
       positions$y[(seq(3,nrow(positions),4))] <- positions$y[(seq(3,nrow(positions),4))] + yWidth/2
       positions$y[(seq(4,nrow(positions),4))] <- positions$y[(seq(4,nrow(positions),4))] - yWidth/2
       values <- data.frame(id = ids,value = c(t(modflow_2d_array*mask^2)))
-      if(!is.null(prj))
-      {
+      if(!is.null(prj)) {
         new_positions <- convert_dis_to_real(x=positions$x,y=positions$y,prj=prj)
         positions$x <- new_positions$x
         positions$y <- new_positions$y
       }
-      if(!is.null(target_CRS))
-      {
-        new_positions <- addosmmerc(data.frame(x=positions$x,y=positions$y),CRS_from=CRS(prj$projection),CRS_to=target_CRS)
-        positions$x <- new_positions$Xosmmerc
-        positions$y <- new_positions$Yosmmerc                    
+      if(!is.null(target_CRS)) {
+        positions <- convert_coordinates(positions,from=CRS(prj$projection),to=target_CRS)                    
       }
       datapoly <- merge(values, positions, by=c("id"))
       datapoly <- na.omit(datapoly)
-      if(add)
-      {
+      if(add) {
         return(geom_polygon(aes(x=x,y=y,group=id),data=datapoly,alpha=alpha,colour='black',fill=NA))# +
         #scale_fill_gradientn(colours=colour_palette(nlevels),limits=zlim)) # solve this issue!
       } else {
@@ -122,9 +107,7 @@ plot.modflow_2d_array <- function(modflow_2d_array, dis, ba6=NULL, mask=ifelse0(
                  geom_polygon(aes(group=id),alpha=alpha,colour='black',fill=NA) +
                  coord_equal())
       }
-    }
-    if(type=='contour')
-    {
+    } else if(type=='contour') {
       xy$z <- c(t(modflow_2d_array*mask^2))
       xyBackup <- xy
       xy <- na.omit(xy)
@@ -137,15 +120,18 @@ plot.modflow_2d_array <- function(modflow_2d_array, dis, ba6=NULL, mask=ifelse0(
       closestGridPoints <- apply(xy[,c('x','y')],1,function(x) which.min((x[1]-xyBackup$x)^2 + (x[2]-xyBackup$y)^2))
       xy$z[which(is.na(xyBackup$z[closestGridPoints]))] <- NA
       rm(xyBackup)
-      if(add)
-      {
+      if(add) {
         if(label) return(stat_contour(aes(x=x,y=y,z=z,colour = ..level..),data=xy,binwidth=binwidth)) # issue: direct.label does not work on ggplot layers, or at least not after fill plot # +geom_dl(aes(label=..level..),method="top.pieces", stat="contour")
         if(!label) return(stat_contour(aes(x=x,y=y,z=z),colour='black',data=xy,binwidth=binwidth))
       } else {
-        if(label) return(ggplot(xy, aes(x=x, y=y, z=z)) + stat_contour(aes(colour = ..level..),binwidth=binwidth) +geom_dl(aes(label=..level.., colour=..level..),method="top.pieces", stat="contour") + coord_equal() +
-                           theme(legend.position="none"))
-        if(!label) return(ggplot(xy, aes(x=x, y=y, z=z)) + stat_contour(colour = 'black',binwidth=binwidth) + coord_equal())
+        if(label) {
+          return(ggplot(xy, aes(x=x, y=y, z=z)) + stat_contour(aes(colour = ..level..),binwidth=binwidth) +geom_dl(aes(label=..level.., colour=..level..),method="top.pieces", stat="contour") + coord_equal() +theme(legend.position="none"))
+        } else {
+          return(ggplot(xy, aes(x=x, y=y, z=z)) + stat_contour(colour = 'black',binwidth=binwidth) + coord_equal())
+        }
       }
+    } else {
+      stop('Please provide valid plot type.')
     }
   }
 }
