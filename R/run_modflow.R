3b49906aaec20b6d4e6752df24c87063b9962024
Bart Rogiers <brogiers@sckcen.be>
2015-11-12 23:03:15 +0100

    Enabled running mf2005 with wine in linux

--- R/run_modflow.R
+++ R/run_modflow.R
@@ -6,8 +6,7 @@
 #' @param modflow_executable name of the MODFLOW executable to use
 #' @param par vector of parameter value file parameter values to run the model with
 #' @export
-run_modflow <- function(file,modflow_executable='mf2005',par=NULL)
-{
+run_modflow <- function(file,modflow_executable=ifelse(Sys.info()['sysname']=='Linux','wine mf2005.exe','mf2005'),par=NULL) {
   dir <- dirname(file)
   file <- basename(file)
   if(!is.null(par))
@@ -17,5 +16,6 @@ run_modflow <- function(file,modflow_executable='mf2005',par=NULL)
     pvl$Parval <- par
     write_pvl(pvl, file=paste0(dir,'/',nam$Fname[which(nam$Ftype=='PVAL')]))
   }
-  shell(paste('cd',dir,'&',modflow_executable,file),mustWork=TRUE)
+  if(Sys.info()['sysname']=='Linux') system(paste('cd',dir,'&',modflow_executable,file))
+  if(Sys.info()['sysname']=='Windows') shell(paste('cd',dir,'&',modflow_executable,file),mustWork=TRUE)
 }
