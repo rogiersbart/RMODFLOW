86612610c1f7262eb69204914f02b82e7ed554e0
rogiersbart <brogiers@sckcen.be>
2015-11-19 10:36:17 +0100

    Updated cell_centers functions to provide x, y and z coordinates

--- R/cell_centers.dis.R
+++ R/cell_centers.dis.R
@@ -1,4 +1,4 @@
-#' Get cell center z coordinates from a dis object
+#' Get cell center x, y and z coordinates from a dis object
 #' 
 #' @param dis dis object
 #' @return 3d array with cell center z coordinates
@@ -8,11 +8,16 @@
 #' @export
 cell_centers.dis <- function(dis)
 {
-  cell_centers <- dis$BOTM*NA
-  cell_centers[,,1] <- (dis$TOP+dis$BOTM[,,1])/2
-  for(k in 2:dis$NLAY)
-  {
-    cell_centers[,,k] <- (dis$BOTM[,,(k-1)]+dis$BOTM[,,k])/2
+  cell_centers <- NULL
+  cell_centers$z <- dis$BOTM*NA
+  cell_centers$z[,,1] <- (dis$TOP+dis$BOTM[,,1])/2
+  for(k in 2:dis$NLAY) {
+    cell_centers$z[,,k] <- (dis$BOTM[,,(k-1)]+dis$BOTM[,,k])/2
   }
+  class(cell_centers$z) <- 'modflow_3d_array'
+  cell_centers$x <- cell_centers$z*0
+  cell_centers$y <- cell_centers$z*0
+  cell_centers$y[,,] <- rev(cumsum(rev(dis$DELC))-rev(dis$DELC)/2)
+  cell_centers$x[,,] <- rep(c(cumsum(dis$DELR)-dis$DELR/2),each=dis$NROW)
   return(cell_centers)
 }
--- R/cell_centers.huf.R
+++ R/cell_centers.huf.R
@@ -6,8 +6,16 @@
 #' @rdname cell_centers
 #' @method cell_centers huf
 #' @export
-cell_centers.huf <- function(huf)
+cell_centers.huf <- function(huf, dis = NULL)
 {
-  cell_centers <- huf$TOP - huf$THCK/2
+  cell_centers <- NULL
+  cell_centers$z <- huf$TOP - huf$THCK/2
+  class(cell_centers$z) <- 'modflow_3d_array'
+  if(!is.null(dis)) {
+    cell_centers$x <- cell_centers$z*0
+    cell_centers$y <- cell_centers$z*0
+    cell_centers$y[,,] <- rev(cumsum(rev(dis$DELC))-rev(dis$DELC)/2)
+    cell_centers$x[,,] <- rep(c(cumsum(dis$DELR)-dis$DELR/2),each=dis$NROW)
+  }
   return(cell_centers)
 }
