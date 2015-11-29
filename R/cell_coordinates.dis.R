f5068f3a76be022f6d009f864dc2240c1336eed4
rogiersbart <brogiers@sckcen.be>
2015-11-29 21:55:18 +0100

    Added option to include face coordinates

--- R/cell_coordinates.dis.R
+++ R/cell_coordinates.dis.R
@@ -6,7 +6,7 @@
 #' @rdname cell_coordinates
 #' @method cell_coordinates dis
 #' @export
-cell_coordinates.dis <- function(dis)
+cell_coordinates.dis <- function(dis, include_faces = FALSE)
 {
   cell_coordinates <- NULL
   cell_coordinates$z <- dis$BOTM*NA
@@ -19,5 +19,13 @@ cell_coordinates.dis <- function(dis)
   cell_coordinates$y <- cell_coordinates$z*0
   cell_coordinates$y[,,] <- rev(cumsum(rev(dis$DELC))-rev(dis$DELC)/2)
   cell_coordinates$x[,,] <- rep(c(cumsum(dis$DELR)-dis$DELR/2),each=dis$NROW)
+  if(include_faces) {
+    cell_coordinates$lower <- dis$BOTM
+    cell_coordinates$upper <- 2 * cell_coordinates$z - dis$BOTM
+    cell_coordinates$left <- cell_coordinates$x - dis$DELR/2
+    cell_coordinates$right <- cell_coordinates$x + dis$DELR/2 
+    cell_coordinates$front <- cell_coordinates$y - rev(dis$DELC)/2
+    cell_coordinates$back <- cell_coordinates$y + rev(dis$DELC)/2
+  }
   return(cell_coordinates)
 }
