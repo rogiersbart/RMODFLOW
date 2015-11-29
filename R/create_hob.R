4e43b25ce702871ab9029b3a2744f0d364a6f26c
rogiersbart <brogiers@sckcen.be>
2015-11-29 21:55:52 +0100

    Updated to first working version of create_hob and write_hob

--- R/create_hob.R
+++ R/create_hob.R
@@ -2,141 +2,96 @@
 #' 
 #' \code{create_hob} creates an RMODFLOW hob object
 #' 
-#' @param xFilter x coordinates of the monitoring well filter
-#' @param yFilter y coordinates of the monitoring well filter
-#' @param topFilter z coordinate of the top of the filter
-#' @param bottomFilter z coordinate of the bottom of the filter
-#' @param head
-#' @param i
-#' @param j
-#' @param xGrid
-#' @param yGrid
-#' @param multiLayer
-#' @param dis
-#' @param useTransmissivity
-#' @param huf
-#' @param mlt
-#' @return Object of class hob
+#' @param locations data.frame with the name, x and y coordinates, and top and bottom of the monitoring well filter
+#' @param time_series data.frame with the name of the monitoring well filter and the observation time and head
+#' @param dis RMODFLOW dis object
+#' @param hydraulic_conductivity 3d array with hydraulic conductivity values, for calculating the weights for a multilayer observation
+#' @param prj RMODFLOW prj object; provide if coordinates in locations are real world coordinates
+#' @param IUHOBSV file unit for saving observation data in a file; the file unit should be included as DATA type in the name file; defaults to 0 (\emph{i.e.} no observation output file will be written)
+#' @param HOBDRY value of the simulated equivalent for a dry cell used in the observation output file
+#' @param NOPRINT logical; if TRUE, the input and output are not printed in the listing file
+#' @param TOMULT time offset multiplier, to convert the time unit in time_series to the MODFLOW time unit
+#' @param unique_obsnam logical; should an ID number be added to OBSNAM for filters with observations at different times? Defaults to FALSE.
+#' @return RMODFLOW hob object
 #' @export
 #' @seealso \code{\link{read.hob}} and \code{\link{write.hob}}
-create_hob <- function(
-  # points in space
-    name, 
-    x,
-    y,
-    z=NULL,
-    top=NULL,
-    bottom=NULL,
-  # points in time
-    head_name,
-    head,
-    time = x * 0,
-    STATISTIC = rep(1, length(x)),
-    STAT_FLAG = rep(0, length(x)),
-    PLOT_SYMBOL = rep(1, length(x)),
-  # other parameters
-    dis,
-    MAXM = dis$NLAY,
-    IUHOBSV = 0,
-    HOBDRY = -999,
-    NOPRINT = TRUE,
-    TOMULTH = 1,
-    prj = NULL,
-    useTransmissivity=F,
-    huf=NA,
-    mlt=NA) {
+create_hob <- function(locations,
+                       time_series,
+                       dis,
+                       hydraulic_conductivity = array(1, dim = c(dis$NROW, dis$NCOL, dis$NLAY)),
+                       IUHOBSV = 0,
+                       HOBDRY = -888,
+                       NOPRINT = FALSE,
+                       TOMULTH = 1,
+                       unique_obsnam = FALSE,
+                       prj = NULL) {
   
   hob <- NULL
   
-#   # Data set 0
-#     # comments should be provided with ?comment
-#   
-#   # Data set 1
-#     hob$NH <- length(x)
-#     hob$MOBS <- ifelse(is.null(top),0,length(which(!is.na(top))))
-#     hob$MAXM <- MAXM
-#     hob$IUHOBSV <- IUHOBSV
-#     hob$HOBDRY <- HOBDRY
-#     hob$NOPRINT <- NOPRINT
-#   
-#   # Data set 2
-#     hob$TOMULT <- TOMULTH
-#     # hob$EVH # MODFLOW-2000
-#   
-#   # Data set 3-6
-#   
-#     # loop over name
-#       {
-#   hob$OBSNAM <- name
-#   locations <- convert_real_to_dis(x = x, y = y, z = z, dis = dis, prj = prj)
-#   hob$LAYER <- locations$k
-#   hob$ROW <- locations$i
-#   hob$COLUMN <- locations$j
-#   first_greater_than <- function(x, y) which(y > x)[1]
-#   hob$IREFSP <- apply(array(time,dim=c(length(time))),1,first_greater_than,cumsum(dis$PERLEN))
-#   hob$ROFF <- locations$roff
-#   hob$COFF <- locations$coff
-#   hob$TOFFSET <- time - c(0,cumsum(dis$PERLEN)[1:(dis$NPER-1)])[hob$IREFSP]
-#   hob$HOBS <- head
-#   hob$STATISTIC <- STATISTIC
-#   hob$STAT_FLAG <- STAT_FLAG
-#   hob$PLOT_SYMBOL <- PLOT_SYMBOL
-#   
-#   ROW <- NULL
-#   COLUMN <- NULL
-#   LAYER <- NULL
-#   ROFF <- NULL
-#   COFF <- NULL
-#   for(h in 1:nrow(xyz)) {
-#     nr <- which.min(points.distance(xyz$x[h],xyz$y[h],xyzGrid$x,xyzGrid$y))
-#     ROW[h] <- xyzGrid$i[nr]
-#     COLUMN[h] <- xyzGrid$j[nr]
-#     ROFF[h] <- -(xyz$y[h]-xyzGrid$y[nr])/50
-#     COFF[h] <- (xyz$x[h]-xyzGrid$x[nr])/50
-#     LAYER[h] <- which(newDIS$BOTM[ROW[h],COLUMN[h],] <= xyz$z[h])[1]
-#   }
-#   
-#   # new MLAY and PR objects
-#   for(h in 1:newHOB$NH) {
-#     # get layers and thicknesses from newDIS file
-#     layerBottoms <- newDIS$BOTM[hobData$ROW[h],hobData$COLUMN[h],]
-#     layerTops <- newDIS$BOTM[hobData$ROW[h],hobData$COLUMN[h],]
-#     layerTops[2:length(layerTops)] <- layerTops[1:(length(layerTops)-1)]
-#     layerTops[1] <- newDIS$TOP[hobData$ROW[h],hobData$COLUMN[h]]
-#     filterTop <- hobData$FilterTop[h]
-#     filterBottom <- hobData$FilterBottom[h]
-#     newHOB$MLAY[[h]] <- which(layerBottoms < filterTop & layerTops > filterBottom)
-#     thicknesses <- NULL
-#     layerCenters <- NULL
-#     for(i in 1:length(newHOB$MLAY[[h]])) {
-#       lay <- newHOB$MLAY[[h]][i]
-#       thicknesses[i] <- min(filterTop,layerTops[lay]) - max(filterBottom, layerBottoms[lay])
-#       layerCenters[i] <- mean(layerTops[lay],layerBottoms[lay])
-#     }
-#     # get kvalues from newHUF and newMLT files
-#     kvalues <- NULL
-#     hufTops <- newHUF$TOP[hobData$ROW[h],hobData$COLUMN[h],]
-#     for(i in 1:length(newHOB$MLAY[[h]])) {
-#       hufNr <- which(hufTops >= layerCenters[i])[1]
-#       kvalues[i] <- newMLT$RMLT[[(2*i)-1]][hobData$ROW[h],hobData$COLUMN[h]]
-#     }
-#     
-#     # weighting according to transmissivity
-#     newHOB$PR[[h]] <- thicknesses * kvalues
-#     newHOB$PR[[h]] <- newHOB$PR[[h]]/sum(newHOB$PR[[h]])
-#   }
-#   
-#   # new MAXM, MOBS and LAYER objects
-#   lengths <- NULL
-#   for(h in 1:newHOB$NH) lengths[h] <- length(newHOB$MLAY[[h]])
-#   newHOB$MAXM <- max(lengths)
-#   newHOB$MOBS <- length(which(lengths > 1))
-#   newHOB$LAYER <- -lengths
-#   singleLayers <- which(newHOB$LAYER==-1)
-#   for(i in singleLayers) newHOB$LAYER[i] <- newHOB$MLAY[[i]]  
-#   
-# }
-#   
-  # update hob$MAXM?
+  # Data set 0
+    # comments should be provided with ?comment
+  
+  # Data set 1
+    hob$NH <- nrow(time_series)
+    hob$MOBS <- NA # set later
+    hob$MAXM <- NA # set later
+    hob$IUHOBSV <- IUHOBSV
+    hob$HOBDRY <- HOBDRY
+    hob$NOPRINT <- NOPRINT
+  
+  # Data set 2
+    hob$TOMULTH <- TOMULTH
+    # hob$EVH # MODFLOW-2000
+  
+  # Data set 3-6
+  
+    hob$OBSNAM <- hob$TOFFSET <- hob$HOBS <- hob$MLAY <- hob$PR <- hob$IREFSP <- list()
+    locations_top <- cbind(locations[,c('x','y','top')],convert_real_to_dis(x = locations$x, y = locations$y, z = locations$top, dis = dis, prj = prj)[,c('i','j','k','roff','coff')])
+    locations_bottom <- cbind(locations[,c('x','y','bottom')],convert_real_to_dis(x = locations$x, y = locations$y, z = locations$bottom, dis = dis, prj = prj)[,c('i','j','k','roff','coff')])
+    for(i in 1:nrow(locations)) {
+      
+      hob$ROW[i] <- locations_top$i[i]
+      hob$COLUMN[i] <- locations_top$j[i]
+      first_greater_than <- function(x, y) which(y > x)[1]
+      if(sum(time_series$name == locations$name[i]) > 1) hob$IREFSP[[i]] <- -sum(time_series$name == locations$name[i])
+      if(sum(time_series$name == locations$name[i]) == 1) hob$IREFSP[[i]] <- first_greater_than(time_series$time[which(time_series$name == locations$name[i])],cumsum(dis$PERLEN))
+      hob$ROFF[i] <- locations_top$roff[i]
+      hob$COFF[i] <- locations_top$coff[i]
+      if(locations_top$k[i] == locations_bottom$k[i]) {
+        hob$LAYER[i] <- locations_top$k[i]
+      } else {
+        hob$LAYER[i] <- - (locations_bottom$k[i] - locations_top$k[i] + 1)
+      }
+      hob$MLAY[[i]] <- hob$PR[[i]] <- NA
+      if(hob$LAYER[i] < 0) {
+        hob$MLAY[[i]] <- locations_top$k[i]:locations_bottom$k[i]
+        length_in_cell <- rep(NA, abs(hob$LAYER[[i]]))
+        for(j in 1:abs(hob$LAYER[[i]])) {
+          layer_tops <- cell_coordinates(dis, include_faces = TRUE)$upper
+          length_in_cell[j] <- min(locations$top[i], layer_tops[locations_top$i[i],locations_top$j[i],hob$MLAY[[i]][j]]) - max(locations$bottom[i], dis$BOTM[locations_top$i[i],locations_top$j[i],hob$MLAY[[i]][j]])
+        }
+        hob$PR[[i]] <- hydraulic_conductivity[locations_top$i[i],locations_top$j[i],hob$MLAY[[i]]] * length_in_cell
+        hob$PR[[i]] <- hob$PR[[i]]/sum(hob$PR[[i]])
+      }
+      if(hob$IREFSP[[i]] > 0) {
+        hob$TOFFSET[[i]] <- time_series$time[which(time_series$name == locations$name[i])] - c(0,cumsum(dis$PERLEN)[1:(dis$NPER-1)])[hob$IREFSP[[i]]]
+        hob$HOBS[[i]] <- time_series$head[which(time_series$name == locations$name[i])]
+        hob$OBSNAM[[i]] <- locations$name[i]
+      } else {
+        hob$ITT[i] <- 1 # to do: enable suppport for ITT = 2!
+        if(unique_obsnam) {
+          hob$OBSNAM[[i]] <- paste(locations$name[i], c(1:abs(hob$IREFSP[[i]])), sep = '_')
+        } else {
+          hob$OBSNAM[[i]] <- rep(locations$name[i], abs(hob$IREFSP[[i]]))
+        }        
+        hob$IREFSP[[i]] <- apply(array(time_series$time[which(time_series$name == locations$name[i])],dim=c(length(time_series$time[which(time_series$name == locations$name[i])]))),1,first_greater_than,cumsum(dis$PERLEN))
+        hob$TOFFSET[[i]] <- time_series$time[which(time_series$name == locations$name[i])] - c(0,cumsum(dis$PERLEN)[1:(dis$NPER-1)])[hob$IREFSP[[i]]]
+        hob$HOBS[[i]] <- time_series$head[which(time_series$name == locations$name[i])]
+      }
+  }
+  
+  hob$MOBS <- length(which(hob$LAYER < 0))
+  hob$MAXM <- abs(min(hob$LAYER[which(hob$LAYER <= 1)]))
+
   return(hob)
 }
--- R/write_hob.R
+++ R/write_hob.R
@@ -15,32 +15,24 @@ write_hob <- function(hob, file)
     write_modflow_variables(hob$NH, hob$MOBS, hob$MAXM, hob$IUHOBSV, hob$HOBDRY, ifelse(hob$NOPRINT,'NOPRINT',''), file=file)
   
   # Data set 2
-    write_modflow_variables(hob$TOMULTH, ifelse(is.na(hob$EVH),1,hob$EVH), file=file)
+    # write_modflow_variables(hob$TOMULTH, ifelse(is.na(hob$EVH),1,hob$EVH), file=file) # MODFLOW-2000
+    write_modflow_variables(hob$TOMULTH, file=file)
   
   # Data set 3 - 6
-    if(class(hob$IREFSP)=='list') {
-      IREFSP <- hob$IREFSP   
-      hob$IREFSP <- NULL
-      for(i in 1:hob$NH) hob$IREFSP[i] <- -length(IREFSP[[i]])
-    }  
-    for(nr in 1:hob$NH) {
-      write_modflow_variables(hob$OBSNAM[[nr]][1], hob$LAYER[nr], hob$ROW[nr], hob$COLUMN[nr], hob$IREFSP[nr], hob$TOFFSET[[nr]][1],hob$ROFF[nr], hob$COFF[nr], hob$HOBS[[nr]][1],ifelse(is.na(hob$STATISTIC[nr]),'',hob$STATISTIC[nr]),ifelse(is.na(hob$STATFLAG[nr]),'',hob$STATFLAG[[nr]][1]),ifelse(is.na(hob$PLOTSYMBOL[[nr]][1]),'',hob$PLOTSYMBOL[[nr]][1]),file=file)
-      if(hob$LAYER[nr] < 0) {
-        line <- NULL
-        for(layerNr in 1:abs(hob$LAYER[nr])) {
-          line <- append(line,hob$MLAY[[nr]][layerNr])
-          line <- append(line,hob$PR[[nr]][layerNr])
-        }
-        write_modflow_variables(line, file=file)
+    for(i in 1:length(hob$LAYER)) {
+      write_modflow_variables(hob$OBSNAM[[i]][1], hob$LAYER[i], hob$ROW[i], hob$COLUMN[i], ifelse(length(hob$IREFSP[[i]]) > 1,-length(hob$IREFSP[[i]]),hob$IREFSP[[i]]), hob$TOFFSET[[i]][1],hob$ROFF[i], hob$COFF[i], hob$HOBS[[i]][1],file=file) 
+      if(hob$LAYER[i] < 0) {
+        write_modflow_variables(paste(hob$MLAY[[i]],hob$PR[[i]],collapse=' '), file=file)
       }
-      if(hob$IREFSP[nr] < 0) {
+      if(length(hob$IREFSP[[i]]) > 1) {
+        
         # Data set 5
-          write_modflow_variables(hob$ITT, file=file)
+          write_modflow_variables(hob$ITT[i], file=file)
         
         # Data set 6
-          for(time in 1:abs(hob$IREFSP[nr])) {
-            write_modflow_variables(hob$OBSNAM[[nr]][time], IREFSP[[nr]][time], hob$TOFFSET[[nr]][time],hob$HOBS[[nr]][time], hob$STATh[[nr]][time], hob$STATdd[[nr]][time],hob$STATFLAG[[nr]][time], hob$PLOTSYMBOL[[nr]][time], file=file)
+          for(j in 1:length(hob$IREFSP[[i]])) {
+            write_modflow_variables(hob$OBSNAM[[i]][j], hob$IREFSP[[i]][j], hob$TOFFSET[[i]][j],hob$HOBS[[i]][j], file=file)
           }
-      }       
-    }  
+      }
+    }
 }  
