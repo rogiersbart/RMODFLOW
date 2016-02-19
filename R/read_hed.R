7c611f48a50e16f3f701bc12430fb4046d5d85fb
rogiersbart <brogiers@sckcen.be>
2016-02-19 15:50:19 +0100

    Added support for double precision in read_hed

--- R/read_hed.R
+++ R/read_hed.R
@@ -15,26 +15,54 @@ read_hed <- function(file = {cat('Please select hed file ...\n'); file.choose()}
                      bas = {cat('Please select bas file ...\n'); read_bas(file.choose(), dis = dis)},
                      huf = NULL,
                      convert_hnoflo_to_NA=TRUE,
-                     binary = TRUE) {
+                     binary = TRUE,
+                     precision = 'single') {
   if(binary) {
     if(!is.null(huf)) {
       dis$nlay <- huf$nhuf
     }
+    real_number_bytes <- ifelse(precision == 'single', 4, 8)
     con <- file(file,open='rb')
     hed <- array(NA, dim = c(dis$nrow, dis$ncol, dis$nlay, sum(dis$nstp)))
     attr(hed, 'kstp') <- attr(hed, 'kper') <- attr(hed, 'pertim') <- attr(hed, 'totim') <- attr(hed, 'desc') <- attr(hed, 'ncol') <- attr(hed, 'nrow') <- attr(hed, 'ilay') <- NULL
     
     kstp <- readBin(con,what='integer',n=1)
     kper <- readBin(con,what='integer',n=1)
-    pertim <- readBin(con,what='numeric',n = 1, size = 4)
-    totim <- readBin(con,what='numeric',n = 1, size = 4)
+    pertim <- readBin(con,what='numeric',n = 1, size = real_number_bytes)
+    totim <- readBin(con,what='numeric',n = 1, size = real_number_bytes)
     desc <- readChar(con,nchars=16)
+    if(! desc %in% c('            HEAD',
+                     '        DRAWDOWN',
+                     '      SUBSIDENCE',
+                     '      COMPACTION',
+                     '   CRITICAL HEAD',
+                     '     HEAD IN HGU',
+                     'NDSYS COMPACTION',
+                     '  Z DISPLACEMENT',
+                     ' D CRITICAL HEAD',
+                     'LAYER COMPACTION',
+                     ' DSYS COMPACTION',
+                     'ND CRITICAL HEAD',
+                     'LAYER COMPACTION',
+                     'SYSTM COMPACTION',
+                     'PRECONSOL STRESS',
+                     'CHANGE IN PCSTRS',
+                     'EFFECTIVE STRESS',
+                     'CHANGE IN EFF-ST',
+                     '      VOID RATIO',
+                     '       THICKNESS',
+                     'CENTER ELEVATION',
+                     'GEOSTATIC STRESS',
+                     'CHANGE IN G-STRS')) {
+      close(con)
+      stop('Array description not recognized. Is the file really binary? If so, you could try double precision. If not, set the binary argument to FALSE.')
+    }
     while(length(desc != 0)) {
       ncol <- readBin(con, what = 'integer', n = 1)
       nrow <- readBin(con, what = 'integer', n = 1)
       ilay <- readBin(con, what = 'integer', n = 1)
       stp_nr <- ifelse(kper==1,kstp,cumsum(dis$nstp)[kper-1]+kstp)
-      hed[,,ilay,stp_nr] <- aperm(array(readBin(con,what='numeric',n = ncol * nrow, size = 4),dim=c(ncol, nrow)), c(2, 1))
+      hed[,,ilay,stp_nr] <- aperm(array(readBin(con,what='numeric',n = ncol * nrow, size = real_number_bytes),dim=c(ncol, nrow)), c(2, 1))
       attr(hed, 'kstp')[stp_nr] <- kstp
       attr(hed, 'kper')[stp_nr] <- kper
       attr(hed, 'pertim')[stp_nr] <- pertim
@@ -45,8 +73,8 @@ read_hed <- function(file = {cat('Please select hed file ...\n'); file.choose()}
       attr(hed, 'ilay')[stp_nr] <- ilay
       kstp <- readBin(con,what='integer',n=1)
       kper <- readBin(con,what='integer',n=1)
-      pertim <- readBin(con,what='numeric',n = 1, size = 4)
-      totim <- readBin(con,what='numeric',n = 1, size = 4)
+      pertim <- readBin(con,what='numeric',n = 1, size = real_number_bytes)
+      totim <- readBin(con,what='numeric',n = 1, size = real_number_bytes)
       desc <- readChar(con,nchars=16)
     }
     no_data <- which(is.na(attr(hed, 'kstp')))
@@ -74,6 +102,31 @@ read_hed <- function(file = {cat('Please select hed file ...\n'); file.choose()}
       pertim <- as.numeric(variables[3])
       totim <- as.numeric(variables[4])
       desc <- variables[5]
+      if(! desc %in% c('HEAD',
+                       'DRAWDOWN',
+                       'SUBSIDENCE',
+                       'COMPACTION',
+                       'CRITICAL HEAD', # spaces! fix!
+                       'HEAD IN HGU',
+                       'NDSYS COMPACTION',
+                       'Z DISPLACEMENT',
+                       'D CRITICAL HEAD',
+                       'LAYER COMPACTION',
+                       'DSYS COMPACTION',
+                       'ND CRITICAL HEAD',
+                       'LAYER COMPACTION',
+                       'SYSTM COMPACTION',
+                       'PRECONSOL STRESS',
+                       'CHANGE IN PCSTRS',
+                       'EFFECTIVE STRESS',
+                       'CHANGE IN EFF-ST',
+                       'VOID RATIO',
+                       'THICKNESS',
+                       'CENTER ELEVATION',
+                       'GEOSTATIC STRESS',
+                       'CHANGE IN G-STRS')) {
+        stop('Array description not recognized. Are you sure the file is not binary? If so, make sure you added the keyword LABEL in the output control file.')
+      }
       ncol <- as.numeric(variables[6])
       nrow <- as.numeric(variables[7])
       ilay <- as.numeric(variables[8])
