91d10e8092db8fc04b4a940207a8c701c62d6b4a
rogiersbart <brogiers@sckcen.be>
2016-06-29 16:42:55 +0200

    Corrections to coordinate functions for use in rwptR

--- R/convert_xyz_to_grid.R
+++ R/convert_xyz_to_grid.R
@@ -2,14 +2,18 @@
 #' 
 #' @param x real world x coordinate
 #' @param y real world y coordinate
-#' @param prj prj object
 #' @param z real world z coordinate; optional
+#' @param prj prj object
 #' @param dis dis object; optional
+#' @param output character; containing 'xyz','ijk' and/or 'off' for the return of x, y, z, i, j, k, roff, coff and loff modflow coordinates
 #' @details
-#' If dis is not provided, only x, y and z coordinates are returned. If z is not provided, no third dimension coordinates are returned.
-#' @return data frame with x, y, z, i, j, k, roff, coff and loff modflow coordinates
+#' If dis is not provided, only x, y and z coordinates are returned. If z is not provided, no third dimension coordinates are returned. For the x, y and z modflow coordinates, the origin is placed at the lower left corner of the grid.
+#' @return data frame with modflow coordinates
 #' @export
-convert_xyz_to_grid <- function(x,y,prj=NULL,z=NULL,dis=NULL) {
+convert_xyz_to_grid <- function(x,y,prj=NULL,z=NULL,dis=NULL,output='xyz') {
+  output_xyz <- grepl('xyz',output)
+  output_ijk <- grepl('ijk',output)
+  output_off <- grepl('off',output)
   if(!is.null(prj)) {
     x <- x-prj$origin[1]
     y <- y-prj$origin[2]
@@ -22,15 +26,14 @@ convert_xyz_to_grid <- function(x,y,prj=NULL,z=NULL,dis=NULL) {
   }
   dat <- data.frame(x=x,y=y)
   if(!is.null(z)) dat$z <- z
-  if(!is.null(dis)) {
+  if(output_ijk | output_off) {
+    if(is.null(dis)) error('Please provide dis argument ...')    
     if(ncol(dat)==3) {
       dis$thck <- dis$tops <- dis$botm
       dis$thck[,,1] <- dis$top - dis$botm[,,1]
       dis$tops[,,1] <- dis$top
-      for(k in 2:dis$nlay) {
-        dis$thck[,,k] <- dis$botm[,,(k-1)] - dis$botm[,,k]
-        dis$tops[,,k] <- dis$botm[,,(k-1)]
-      }
+      dis$thck[,,2:dis$nlay] <- dis$botm[,,(2:dis$nlay-1)] - dis$botm[,,2:dis$nlay]
+      dis$tops[,,2:dis$nlay] <- dis$botm[,,(2:dis$nlay-1)]
     }
     for(i in 1:nrow(dat)) {
       dat$i[i] <- which(cumsum(dis$delc) > sum(dis$delc)-dat$y[i])[1]
@@ -42,6 +45,13 @@ convert_xyz_to_grid <- function(x,y,prj=NULL,z=NULL,dis=NULL) {
         dat$loff[i] <- -(dat$z[i]-(dis$tops[dat$i[i],dat$j[i],dat$k[i]]+dis$botm[dat$i[i],dat$j[i],dat$k[i]])/2)/dis$thck[dat$i[i],dat$j[i],dat$k[i]]
       }
     }
+    if(output_xyz & output_ijk & output_off) return(dat)
+    if(output_xyz & output_ijk & !output_off) return(dat[,c('x','y','z','i','j','k')])
+    if(output_xyz & !output_ijk & output_off) return(dat[,c('x','y','z','roff','coff','loff')])
+    if(!output_xyz & output_ijk & output_off) return(dat[,c('i','j','k','roff','coff','loff')])
+    if(!output_xyz & !output_ijk & output_off) return(dat[,c('roff','coff','loff')])
+    if(!output_xyz & output_ijk & !output_off) return(dat[,c('i','j','k')])
+  } else {
+    return(dat)
   }
-  return(dat)
 }
