299f78c2c5f913d0d2589069ee2c5a707543c4e0
rogiersbart <brogiers@sckcen.be>
2016-10-11 13:04:27 +0200

    Added support for face coordinates for huf

--- R/cell_coordinates.huf.R
+++ R/cell_coordinates.huf.R
@@ -1,13 +1,16 @@
 #' Get cell coordinates from a huf object
 #' 
 #' @param huf huf object
+#' @param dis dis object, corresponding to the huf object
+#' @param include_faces logical; should face coordinates be included?
 #' @return 3d array with cell coordinates
 #'
 #' @rdname cell_coordinates
 #' @method cell_coordinates huf
 #' @export
-cell_coordinates.huf <- function(huf, dis = NULL)
-{
+cell_coordinates.huf <- function(huf,
+                                 dis = NULL,
+                                 include_faces = FALSE) {
   cell_coordinates <- NULL
   cell_coordinates$z <- huf$top - huf$thck/2
   class(cell_coordinates$z) <- 'rmodflow_3d_array'
@@ -16,6 +19,16 @@ cell_coordinates.huf <- function(huf, dis = NULL)
     cell_coordinates$y <- cell_coordinates$z*0
     cell_coordinates$y[,,] <- rev(cumsum(rev(dis$delc))-rev(dis$delc)/2)
     cell_coordinates$x[,,] <- rep(c(cumsum(dis$delr)-dis$delr/2),each=dis$nrow)
+  }
+  if(include_faces) {
+    dis$delr <- array(rep(dis$delr,each=dis$nrow),dim=c(dis$nrow,dis$ncol,huf$nhuf))
+    dis$delc <- array(rep(dis$delc,dis$ncol),dim=c(dis$nrow,dis$ncol,huf$nhuf))
+    cell_coordinates$lower <- cell_coordinates$z - huf$thck/2
+    cell_coordinates$upper <- cell_coordinates$z + huf$thck/2
+    cell_coordinates$left <- cell_coordinates$x - dis$delr/2
+    cell_coordinates$right <- cell_coordinates$x + dis$delr/2 
+    cell_coordinates$front <- cell_coordinates$y - dis$delc/2
+    cell_coordinates$back <- cell_coordinates$y + dis$delc/2
   }
   return(cell_coordinates)
 }
