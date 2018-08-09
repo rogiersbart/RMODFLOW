96e5cdcfca5d6af67581a3810fbc8454ecdef776
cneyens <cas.neyens@gmail.com>
2018-08-09 09:56:45 +0200

    Fixed small bugs and deprecated function names

--- R/rmfi-get-balance.R
+++ R/rmfi-get-balance.R
@@ -7,7 +7,7 @@
 rmfi_get_balance=function(balance_lines){ # balance lines is a character vector of length n where each element is a line of a text file with n lines. The first line should be "VOLUMETRIC BUDGET OF THE ENTIRE MODEL AT TIME STEP K, STRESS PERIOD M"; the last line should be "PERCENT DISCREPANCY = ..."
   
   header = balance_lines[1]
-  header_lines = remove_empty_strings(strsplit(header, ' ')[[1]])
+  header_lines = rmfi_remove_empty_strings(strsplit(header, ' ')[[1]])
   
   
   balance_lines_inflow =  balance_lines[(which(grepl('IN:', balance_lines))+2):(which(grepl('OUT:', balance_lines))-2)]
@@ -25,8 +25,8 @@ rmfi_get_balance=function(balance_lines){ # balance lines is a character vector 
   
   for (i in 1:(length(balance_lines_inflow)-2)){
     
-    tmp_in = remove_empty_strings(strsplit(balance_lines_inflow[i], ' ')[[1]])
-    tmp_out = remove_empty_strings(strsplit(balance_lines_outflow[i], ' ')[[1]])
+    tmp_in = rmfi_remove_empty_strings(strsplit(balance_lines_inflow[i], ' ')[[1]])
+    tmp_out = rmfi_remove_empty_strings(strsplit(balance_lines_outflow[i], ' ')[[1]])
     
     balance$df[1, i] = as.numeric(tmp_in[length(tmp_in)/2]) 
     balance$df[3, i] = as.numeric(tmp_in[length(tmp_in)])
@@ -42,11 +42,11 @@ rmfi_get_balance=function(balance_lines){ # balance lines is a character vector 
   colnames(balance$df) = column_names
   
   # vector with the IN - OUT difference
-  difference_lines_split =  remove_empty_strings(strsplit(difference_lines, ' ')[[1]])
+  difference_lines_split =  rmfi_remove_empty_strings(strsplit(difference_lines, ' ')[[1]])
   balance$diff = c('cuml_diff' = as.numeric(difference_lines_split[length(difference_lines_split)/2]), 'rate_diff' =  as.numeric(difference_lines_split[length(difference_lines_split)]))
   
   # vector with the percent discrepancy
-  perc_discrepancy_lines_split = remove_empty_strings(strsplit(perc_discrepancy_lines, ' ')[[1]])
+  perc_discrepancy_lines_split = rmfi_remove_empty_strings(strsplit(perc_discrepancy_lines, ' ')[[1]])
   balance$discr = c('cuml_discr' = as.numeric(perc_discrepancy_lines_split[length(perc_discrepancy_lines_split)/2]), 'rate_discr' = as.numeric(perc_discrepancy_lines_split[length(perc_discrepancy_lines_split)]) )
   
   attributes(balance)=list('sp'= as.numeric(header_lines[length(header_lines)]), 'ts'= as.numeric(paste(strsplit(header_lines[length(header_lines)-3], '')[[1]][1:(which(strsplit(header_lines[length(header_lines)-3], '')[[1]]==',')-1)], collapse="" )) )
