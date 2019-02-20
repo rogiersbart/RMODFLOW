0bb1e4a6c3e8f83a42175d36c64bbbfa47c8f4a6
cneyens <cas.neyens@gmail.com>
2019-02-20 10:53:28 +0100

    Fixed problem with parsing fixed format variables by adding n argument; now returns 0 if last variable is missing from file

--- R/rmfi-parse-variables.R
+++ R/rmfi-parse-variables.R
@@ -1,9 +1,10 @@
 #' Read modflow variables
 #' If all are numbers, returns numeric, otherwise returns character vector
+#' @param n integer; number of variables to be returned. Only used when format is \code{'fixed'}
 #' @param format character, either \code{'free'} or \code{'fixed'}. When 'fixed', reads 10-character fields and converts to numeric. Empty fields are set to zero.
 #' @param ... ignored
 #' @keywords internal
-rmfi_parse_variables <- function(remaining_lines, format = 'free', ...) {
+rmfi_parse_variables <- function(remaining_lines, n, format = 'free', ...) {
   if(format == 'free') {
     variables <- rmfi_remove_empty_strings(strsplit(rmfi_remove_comments_end_of_line(remaining_lines[1]),' |\t|,')[[1]])
     if(!any(is.na(suppressWarnings(as.numeric(variables))))) variables <- as.numeric(variables)
@@ -12,8 +13,11 @@ rmfi_parse_variables <- function(remaining_lines, format = 'free', ...) {
                                 function(i) paste0(strsplit(rmfi_remove_comments_end_of_line(remaining_lines[1]),'')[[1]][i:(i+9)], collapse=''))))
     variables <- lapply(strsplit(variables, " |t"), rmfi_remove_empty_strings)
     variables[which(lengths(variables)==0)] = 0 # empty values are set to 0
-    variables = unlist(variables)
-    if(!any(is.na(suppressWarnings(as.numeric(variables))))) variables <- as.numeric(variables)
+    variables <-  unlist(variables)
+    if(!any(is.na(suppressWarnings(as.numeric(variables))))) {
+      variables <- as.numeric(variables)
+      if(length(variables) < n) variables <- c(variables, rep(0, n - length(variables))) # append 0's if values are missing
+    }
   }
   return(list(variables=variables,remaining_lines= remaining_lines[-1]))
 }
