bf252e7db72915f5229ab57d790029a63ea5e436
cneyens <cas.neyens@gmail.com>
2019-02-20 10:54:37 +0100

    rmfi_write_variables can handle list input; also removes empty elements before writing

--- R/rmfi-write-variables.R
+++ R/rmfi-write-variables.R
@@ -3,9 +3,11 @@
 #' @param format either \code{'fixed'} or \code{'free'}. Fixed format assumes 10 character spaces for each value
 #' @keywords internal
 rmfi_write_variables <- function(..., file, append=TRUE, format = 'free') {
+  arg <- unlist(list(...))
+  arg <- arg[nchar(arg) > 0] # removes empty elements
   if(format == 'free') {
-    cat(paste0(paste(..., sep=' ',collapse=' '), '\n'), file=file, append=append)
+    cat(paste0(paste(arg, sep=' ',collapse=' '), '\n'), file=file, append=append)
   } else if(format == 'fixed') { # optional items are always free format so no need to adjust code for characters
-    cat(paste0(paste0(formatC(unlist(list(...)), width=10),collapse=''), '\n'), file=file, append=append)
+    cat(paste0(paste0(formatC(arg, width=10),collapse=''), '\n'), file=file, append=append)
   } 
 }
