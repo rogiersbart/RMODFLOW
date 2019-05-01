335e0765b6d3e43a32172b3864d0934ebb360f22
cneyens <cas.neyens@gmail.com>
2019-05-01 14:54:21 +0200

    Fixed error for reading 1D(NLAY) variables specified on multiple lines

--- R/rmfi-parse-variables.R
+++ R/rmfi-parse-variables.R
@@ -1,14 +1,15 @@
 #' Read modflow variables
 #' If all are numbers, returns numeric, otherwise returns character vector
-#' @param n integer; number of variables to be returned. Must be specified when format is \code{'fixed'}. Can also be used for \code{'free'} format e.g. when a 1D variable is specified on multiple lines. 
+#' @param n integer; number of variables to be returned. Only used when format is \code{'fixed'}.  
+#' @param nlay integer; number of layers for which values are to be read. Only used when format is \code{'free'} and a 1D(NLAY) variable is read which may be specified on multiple lines.
 #' @param format character, either \code{'free'} or \code{'fixed'}. When 'fixed', reads 10-character fields and converts to numeric. Empty fields are set to zero.
 #' @param ... ignored
 #' @keywords internal
-rmfi_parse_variables <- function(remaining_lines, n = NULL, format = 'free', ...) {
+rmfi_parse_variables <- function(remaining_lines, n, nlay = NULL, format = 'free', ...) {
   if(format == 'free') {
     variables <- rmfi_remove_empty_strings(strsplit(rmfi_remove_comments_end_of_line(remaining_lines[1]),' |\t|,')[[1]])
-    if(!is.null(n)) {
-      while(length(variables) < n) {
+    if(!is.null(nlay)) {
+      while(length(variables) < nlay) { 
         remaining_lines <- remaining_lines[-1]
         variables <- append(variables, rmfi_remove_empty_strings(strsplit(rmfi_remove_comments_end_of_line(remaining_lines[1]),' |\t|,')[[1]]))
       }
