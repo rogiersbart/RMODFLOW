rogiersbart/RMODFLOW

commit 704d01290f7f0e14a44753f55126ad8f8d3a7f7d

Updated functions to account for quasi-3D confining beds

R/rmf-convert-dis-to-saturated-dis.R
@@ -15,7 +15,20 @@ rmf_convert_dis_to_saturated_dis <- function(dis,
       hed <- hed[,,,dim(hed)[4]]
     }
   }
-  dis$botm[,,1:(dis$nlay-1)][which(hed[,,2:(dis$nlay)] < dis$botm[,,1:(dis$nlay-1)])] <- hed[,,2:(dis$nlay)][which(hed[,,2:(dis$nlay)] < dis$botm[,,1:(dis$nlay-1)])]
+  # adjusting confining beds 
+  nnlay <- dis$nlay + length(which(dis$laycbd != 0))
+  cbd <- rep(0, nnlay)
+  cbd[cumsum(dis$laycbd+1)[dis$laycbd != 0]] <- 1
+  if(nnlay > 1) {
+    thck <- botm <- dis$botm
+    thck[,,1] <- dis$top - dis$botm[,,1]
+    thck[,,2:nnlay] <- dis$botm[,,(2:nnlay)-1] - dis$botm[,,2:nnlay]
+    dis$botm <- dis$botm[,,!cbd]
+    dis$botm[,,1:(dis$nlay-1)][which(hed[,,2:(dis$nlay)] < dis$botm[,,1:(dis$nlay-1)])] <- hed[,,2:(dis$nlay)][which(hed[,,2:(dis$nlay)] < dis$botm[,,1:(dis$nlay-1)])]
+    botm[,,!cbd] <- dis$botm
+    botm[,,which(cbd == 1)] <- botm[,,which(cbd == 1)-1] - thck[,,which(cbd == 1)]
+    dis$botm <- botm
+  } 
   dis$top <- hed[,,1]
   return(dis)
 }

R/rmf-cell-coordinates.dis.R
@@ -11,12 +11,19 @@ rmf_cell_coordinates.dis <- function(dis,
   cell_coordinates <- NULL
   cell_coordinates$z <- dis$botm*NA
   cell_coordinates$z[,,1] <- (dis$top+dis$botm[,,1])/2
-  if(dis$nlay > 1) {
-    for(k in 2:dis$nlay) {
+  
+  nnlay <- dis$nlay + length(which(dis$laycbd != 0))
+  if(nnlay > 1) {
+    for(k in 2:nnlay) {
       cell_coordinates$z[,,k] <- (dis$botm[,,(k-1)]+dis$botm[,,k])/2
     }
   }
   class(cell_coordinates$z) <- 'rmf_3d_array'
+  # remove the confining beds
+  cbd <- rep(0, nnlay)
+  cbd[cumsum(dis$laycbd+1)[dis$laycbd != 0]] <- 1
+  cell_coordinates$z <- cell_coordinates$z[,,!cbd]
+  
   cell_coordinates$x <- cell_coordinates$z*0
   cell_coordinates$y <- cell_coordinates$z*0
   cell_coordinates$y[,,] <- rev(cumsum(rev(dis$delc))-rev(dis$delc)/2)
@@ -24,8 +31,8 @@ rmf_cell_coordinates.dis <- function(dis,
   if(include_faces) {
     dis$delr <- array(rep(dis$delr,each=dis$nrow),dim=c(dis$nrow,dis$ncol,dis$nlay))
     dis$delc <- array(rep(dis$delc,dis$ncol),dim=c(dis$nrow,dis$ncol,dis$nlay))
-    cell_coordinates$lower <- dis$botm
-    cell_coordinates$upper <- 2 * cell_coordinates$z - dis$botm
+    cell_coordinates$lower <- dis$botm[,,!cbd]
+    cell_coordinates$upper <- 2 * cell_coordinates$z - dis$botm[,,!cbd]
     cell_coordinates$left <- cell_coordinates$x - dis$delr/2
     cell_coordinates$right <- cell_coordinates$x + dis$delr/2 
     cell_coordinates$front <- cell_coordinates$y - dis$delc/2

R/rmf-cell-dimensions.dis.R
@@ -13,14 +13,18 @@ rmf_cell_dimensions.dis <- function(dis,
                                     include_volume = FALSE,
                                     include_faces = FALSE) {
   cell_dimensions <- list()
+  # remove the confining beds
+  nnlay <- dis$nlay + length(which(dis$laycbd != 0))
+  cbd <- rep(0, nnlay)
+  cbd[cumsum(dis$laycbd+1)[dis$laycbd != 0]] <- 1
   if (is.null(hed) | ifelse(is.null(hed),FALSE,dim(hed)[4] == 1)) {
     cell_top <- dis$botm
     cell_top[,,1] <- dis$top
-    cell_top[,,2:dis$nlay] <- dis$botm[,,c(1:(dis$nlay-1))]
+    if(nnlay > 1) cell_top[,,2:nnlay] <- dis$botm[,,c(1:(nnlay-1))]
     if(!is.null(hed)) {
       cell_top[which(cell_top > hed[,,,1])] <- hed[which(cell_top > hed[,,,1])]
     }
-    cell_dimensions$z <- rmf_create_array(cell_top - dis$botm)
+    cell_dimensions$z <- rmf_create_array(cell_top - dis$botm)[,,!cbd]
     cell_dimensions$x <- rmf_create_array(rep(dis$delc,dis$ncol*dis$nlay),dim=c(dis$nrow,dis$ncol,dis$nlay))
     cell_dimensions$y <- rmf_create_array(rep(dis$delr, dis$nlay, each = dis$nrow),dim=c(dis$nrow,dis$ncol,dis$nlay))
     if(include_volume) cell_dimensions$volume <- rmf_create_array(with(cell_dimensions, x * y * z))
@@ -33,11 +37,11 @@ rmf_cell_dimensions.dis <- function(dis,
       cell_dimensions$upper <- cell_dimensions$lower
     }  
   } else {
-    cell_top <- rmf_create_array(dim = c(dis$nrow, dis$ncol, dis$nlay, sum(dis$nstp)))
+    cell_top <- rmf_create_array(dim = c(dis$nrow, dis$ncol, nnlay, sum(dis$nstp)))
     cell_top[,,1,] <- dis$top
-    cell_top[,,2:dis$nlay,] <- dis$botm[,,c(1:(dis$nlay-1))]
+    if(nnlay > 1) cell_top[,,2:nnlay,] <- dis$botm[,,c(1:(nnlay-1))]
     cell_top[which(cell_top > hed[,,,])] <- hed[which(cell_top > hed[,,,])] 
-    cell_dimensions$z <- rmf_create_array(cell_top - dis$botm)
+    cell_dimensions$z <- rmf_create_array(cell_top - dis$botm)[,,!cbd]
     cell_dimensions$x <- rmf_create_array(rep(dis$delc,dis$ncol*dis$nlay),dim=c(dis$nrow,dis$ncol,dis$nlay))
     cell_dimensions$y <- rmf_create_array(rep(dis$delr, dis$nlay, each = dis$nrow),dim=c(dis$nrow,dis$ncol,dis$nlay))
     if(include_volume) cell_dimensions$volume <- rmf_create_array(with(cell_dimensions, x * y * z))

R/rmf-cell-info.dis.R
@@ -19,8 +19,18 @@ rmf_cell_info.dis <- function(dis,
   cat('\t\t Top \t\t Bottom \t Thickness\n', sep='')
   cat('Layer 1:\t', dis$top[i, j], '\t', dis$botm[i,j,1],'\t', dis$top[i, j]-dis$botm[i,j,1],'\n', sep='')
   
-  for(k in 2:dis$nlay)
-  {
-    cat('Layer ',k,':\t', dis$botm[i, j, k-1], '\t', dis$botm[i,j,k],'\t', dis$botm[i, j, k-1]-dis$botm[i,j,k],'\n', sep='')
+  nnlay <- dis$nlay + length(which(dis$laycbd != 0))
+  if(nnlay > 1) {
+    cbd <- rep(0, nnlay)
+    cbd[cumsum(dis$laycbd+1)[dis$laycbd != 0]] <- 1
+    for(k in 2:nnlay)
+    {
+      if(cbd[k]) {
+        cat('Quasi-3D Confining Bed below layer ',k-1,':\t', dis$botm[i, j, k-1], '\t', dis$botm[i,j,k],'\t', dis$botm[i, j, k-1]-dis$botm[i,j,k],'\n', sep='')
+      } else {
+        cat('Layer ',k,':\t', dis$botm[i, j, k-1], '\t', dis$botm[i,j,k],'\t', dis$botm[i, j, k-1]-dis$botm[i,j,k],'\n', sep='')
+      }
+    }
   }
+
 }

R/rmf-convert-grid-to-xyz.R
@@ -12,7 +12,14 @@
 #' @param prj prj object
 #' @param dis dis object
 #' @details Provide either xyz or ijk
-#' @return data frame with real world x and y coordinates
+#'   If xyz is provided, it is reprojected using the optional prj object.
+#'   
+#'   Optional roff, coff and loff values can be supplied along the i, j and k indices.
+#'   
+#'   k indices should not represent Quasi-3D confining beds. If a real world coordinate should be obtained from a Quasi-3D confining bed,
+#'   set the k index to the overlying model layer and supply a loff value (relative to the thickness of the overlying model layer)
+#'   
+#' @return data frame with real world x and y (and optionally z) coordinates
 #' @export
 rmf_convert_grid_to_xyz <- function(x = NULL,
                                     y = NULL,
@@ -23,29 +30,53 @@ rmf_convert_grid_to_xyz <- function(x = NULL,
                                     roff = NULL,
                                     coff = NULL,
                                     loff = NULL,
-                                    prj,
+                                    prj = NULL,
                                     dis = NULL) {
   if(!is.null(x)) {
-    if(length(prj$origin) <= 2) prj$origin <-  c(prj$origin, 0)
-    
-    # counterclockwise rotation is negative. Now corresponds to rmf_convert_xyz_to_grid
-    angle <- atan(y/x)*180/pi+prj$rotation
-    angle[which(is.na(angle))] <- 90-prj$rotation
-    s <- sqrt(x^2+y^2)
-    x <- prj$origin[1]+ cos(angle*pi/180)*s
-    y <- prj$origin[2]+ sin(angle*pi/180)*s
-    if(!is.null(z)) z <- prj$origin[3]+z
+    if(!is.null(prj)) {
+      if(length(prj$origin) <= 2) prj$origin <-  c(prj$origin, 0)
+      # counterclockwise rotation is negative. Now corresponds to rmf_convert_xyz_to_grid
+      angle <- atan(y/x)*180/pi+prj$rotation
+      angle[which(is.na(angle))] <- 90-prj$rotation
+      s <- sqrt(x^2+y^2)
+      x <- prj$origin[1]+ cos(angle*pi/180)*s
+      y <- prj$origin[2]+ sin(angle*pi/180)*s
+      if(!is.null(z)) z <- prj$origin[3]+z
+    }
     ifelse(!is.null(z),return(data.frame(x=x,y=y,z=z)),return(data.frame(x=x,y=y)))
-  } else if(!is.null(i)) {
-    #  vectorize
+ 
+   } else if(!is.null(i)) {
+    if(is.null(dis)) stop('Please provide a dis object')
     y_grid <- c(cumsum(rev(dis$delc))-rev(dis$delc)/2)[(dis$nrow-i+1)]
     x_grid <- c(cumsum(dis$delr)-dis$delr/2)[j]
-    if(!is.null(k)) { # need to adjust nlay for semi-confining beds
-      thck <-  dis$botm
+    
+    if(!is.null(k)) {
+      
+      # set thicknesses of cells
+      if(any(k > dis$nlay)) stop('k is greater than dis$nlay')
+      thck <- dis$botm
+      nnlay <- ifelse(length(dim(dis$botm)) > 2, dim(dis$botm)[3], 1)
+      if(nnlay > 1) thck[,,2:nnlay] <- dis$botm[,,(2:nnlay)-1]-dis$botm[,,2:nnlay]
       thck[,,1] <- dis$top - dis$botm[,,1]
-      thck[,,2:dis$nlay] <- dis$botm[,,(2:dis$nlay-1)] - dis$botm[,,2:dis$nlay]
-      z_grid <- sum(thck[i,j,k:dis$nlay])-thck[i,j,k]/2
-      if(!is.null(loff)) z_grid <- z_grid - loff*thck[i,j,k]/2
+      
+      #  vectorize this:
+      
+      # adjust botm for presence of confining beds
+      df <- data.frame(i=i,j=j,k=k)
+      if(!is.null(loff)) df$loff <- loff
+      cbd <- rep(0, nnlay)
+      cbd[cumsum(dis$laycbd+1)[dis$laycbd != 0]] <- 1
+      
+      for(x in 1:nrow(df)) {
+        k_adj <- ifelse(df$k[x] == 1, df$k[x], df$k[x] + sum((dis$laycbd != 0)[1:(df$k[x]-1)]))
+        cell_thchk <- sum(thck[df$i[x],df$j[x],k_adj:nnlay])
+        # calculate z of cell center; normalize with bottom of model (which can never be a confined bed)
+        df$z[x] <- cell_thchk-thck[,,!cbd][df$i[x],df$j[x],df$k[x]]/2 + dis$botm[df$i[x],df$j[x],nnlay]
+        if(!is.null(loff)) df$z[x] <- df$z[x] - df$loff[x]*thck[df$i[x],df$j[x],df$k[x]]
+        
+      }
+      z_grid <- df$z
+      rm(df)
     }
     
     # offset
@@ -70,7 +101,7 @@ rmf_convert_grid_to_xyz <- function(x = NULL,
     }
     
     dat <- data.frame(x=x,y=y)
-    if(!is.null(k)) dat$z = z
+    if(!is.null(k)) dat <- data.frame(x=x,y=y,z=z)
     return(dat)
   }
 }                                                                           

R/rmf-convert-huf-to-grid.R
@@ -12,6 +12,7 @@ rmf_convert_huf_to_grid <- function(values,
                                     dis,
                                     mask = dis$top / dis$top,
                                     type = 'arithmetic') {
+  if(any(dis$laycbd != 0)) warning('Using Quasi-3D confining beds as explicit layers')
   num_grid_array <- dis$botm*NA
   huf$botm <- huf$thck*NA
   huf$botm[,,1] <- huf$top[,,1]-huf$thck[,,1]

R/rmf-convert-huf-to-nlay.R
@@ -8,6 +8,11 @@
 rmf_convert_huf_to_nlay <- function(huf, dis, bas) {
   nlay <- huf$top * 0
   huf_coordinates <- rmf_cell_coordinates(huf, dis = dis, include_faces = TRUE)
+  if(any(dis$laycbd != 0)) {
+    warning('Using Quasi-3D confining beds as explicit layers')
+    dis$nlay <- dis$nlay + length(which(dis$laycbd != 0))
+    dis$laycbd <- rep(0, dis$nlay)
+  }
   dis_coordinates <- rmf_cell_coordinates(dis, include_faces = TRUE)
   ibound <- abs(bas$ibound)
   for(i in 1:huf$nhuf) {

R/rmf-convert-xyz-to-grid.R
@@ -8,12 +8,15 @@
 #' @param output character; containing 'xyz','ijk' and/or 'off' for the return of x, y, z, i, j, k, roff, coff and loff modflow coordinates
 #' @details
 #' If dis is not provided, only x, y and z coordinates are returned. If z is not provided, no third dimension coordinates are returned. For the x, y and z modflow coordinates, the origin is placed at the lower left corner of the grid.
+#' If the xyz coordinate falls on a boundary of two cells, the minimum ijk indices are returned. 
+#'
+#' If the z coordinate falls within a Quasi-3D confining bed, the layer index of the overlying model layer is returned. The loff value then represents the fractional distance from the center of the overlying model layer.
 #' @return data frame with modflow coordinates
 #' @export
 rmf_convert_xyz_to_grid <- function(x,y,prj=NULL,z=NULL,dis=NULL,output='xyz') {
-  output_xyz <- grepl('xyz',output)
-  output_ijk <- grepl('ijk',output)
-  output_off <- grepl('off',output)
+  output_xyz <- 'xyz' %in% output
+  output_ijk <- 'ijk' %in% output
+  output_off <- 'off' %in% output
   if(!is.null(prj)) {
     if(length(prj$origin) <= 2) prj$origin <-  c(prj$origin, 0)
     x <- x-prj$origin[1]
@@ -27,23 +30,59 @@ rmf_convert_xyz_to_grid <- function(x,y,prj=NULL,z=NULL,dis=NULL,output='xyz') {
   }
   dat <- data.frame(x=x,y=y)
   if(!is.null(z)) dat$z <- z
-  if(output_ijk | output_off) {
+  if(output_ijk || output_off) {
     if(is.null(dis)) stop('Please provide dis argument ...')    
     if(ncol(dat)==3) {
       dis$thck <- dis$tops <- dis$botm
       dis$thck[,,1] <- dis$top - dis$botm[,,1]
       dis$tops[,,1] <- dis$top
-      dis$thck[,,2:dis$nlay] <- dis$botm[,,(2:dis$nlay-1)] - dis$botm[,,2:dis$nlay]
-      dis$tops[,,2:dis$nlay] <- dis$botm[,,(2:dis$nlay-1)]
+      nnlay <- dim(dis$botm)[3] 
+      if(nnlay > 1) dis$thck[,,2:nnlay] <- dis$botm[,,(2:nnlay)-1] - dis$botm[,,2:nnlay]
+      if(nnlay > 1) dis$tops[,,2:nnlay] <- dis$botm[,,(2:nnlay)-1]
+    }
+    if(any(dis$laycbd != 0)) {
+      cbd <- rep(0, nnlay)
+      cbd[cumsum(dis$laycbd+1)[dis$laycbd != 0]] <- 1
     }
     for(i in 1:nrow(dat)) {
-      dat$i[i] <- which(cumsum(dis$delc) > sum(dis$delc)-dat$y[i])[1]
-      dat$j[i] <- which(cumsum(dis$delr) > dat$x[i])[1]
+      dat$i[i] <- which(cumsum(dis$delc) >= sum(dis$delc)-dat$y[i])[1]
+      dat$j[i] <- which(cumsum(dis$delr) >= dat$x[i])[1]
       dat$roff[i] <- (sum(dis$delc)-dat$y[i] -(cumsum(dis$delc) - dis$delc/2)[dat$i[i]])/dis$delc[dat$i[i]]
       dat$coff[i] <- (dat$x[i] -(cumsum(dis$delr) - dis$delr/2)[dat$j[i]])/dis$delr[dat$j[i]]
+      
+      if(dat$x[i] < 0 || dat$x[i] > sum(dis$delr)) {
+        dat$j[i] <- NA
+        dat$roff[i] <- NA
+        dat$coff[i] <- NA
+        warning('x coordinate out of bounds. Setting j index and roff/coff to NA')
+      } 
+      if(dat$y[i] < 0 || dat$y[i] > sum(dis$delc)) {
+        dat$i[i] <- NA
+        dat$roff[i] <- NA
+        dat$coff[i] <- NA
+        warning('y coordinate out of bounds. Setting i index and roff/coff to NA')
+      }
+
       if(!is.null(z)) {
-        dat$k[i] <- which(dis$botm[dat$i[i],dat$j[i],] < dat$z[i])[1]
-        dat$loff[i] <- -(dat$z[i]-(dis$tops[dat$i[i],dat$j[i],dat$k[i]]+dis$botm[dat$i[i],dat$j[i],dat$k[i]])/2)/dis$thck[dat$i[i],dat$j[i],dat$k[i]]
+        if(is.na(dat$i[i]) || is.na(dat$j[i])) {
+          warning('i and/or j index is NA. Setting corresponding k index and loff to NA as well.')
+          dat$k[i] <- NA
+          dat$loff[i] <- NA
+        } else if(dat$z[i] < dis$botm[dat$i[i], dat$j[i],nnlay] || dat$z[i] > dis$top[dat$i[i], dat$j[i]]) {
+          warning('z coordinate out of bounds. Setting k index and loff to NA.')
+          dat$k[i] <- NA
+          dat$loff[i] <- NA
+        } else {
+          dat$k[i] <- which(dis$botm[dat$i[i],dat$j[i],] <= dat$z[i])[1]
+          k_org <- dat$k[i]
+          
+          # adjust for cbd
+          if(any(dis$laycbd != 0)) {
+            dat$k[i] <- dat$k[i] - sum(cbd[1:dat$k[i]])
+            if(dis$laycbd[dat$k[i]] != 0) k_org <- dat$k[i]
+          }
+          dat$loff[i] <- -(dat$z[i]-(dis$tops[dat$i[i],dat$j[i],k_org]+dis$botm[dat$i[i],dat$j[i],k_org])/2)/dis$thck[dat$i[i],dat$j[i],k_org]
+        }
       }
     }
     

commit 7dae6878474eab89589b3aba416c4b0513e9cd2d

Fix when confining beds are not present
Bug introduced in 704d012

R/rmf-convert-dis-to-saturated-dis.R
@@ -15,7 +15,7 @@ rmf_convert_dis_to_saturated_dis <- function(dis,
       hed <- hed[,,,dim(hed)[4]]
     }
   }
-  # adjusting confining beds 
+  # adjusting confining beds  - REVIEW required
   nnlay <- dis$nlay + length(which(dis$laycbd != 0))
   cbd <- rep(0, nnlay)
   cbd[cumsum(dis$laycbd+1)[dis$laycbd != 0]] <- 1
@@ -26,7 +26,7 @@ rmf_convert_dis_to_saturated_dis <- function(dis,
     dis$botm <- dis$botm[,,!cbd]
     dis$botm[,,1:(dis$nlay-1)][which(hed[,,2:(dis$nlay)] < dis$botm[,,1:(dis$nlay-1)])] <- hed[,,2:(dis$nlay)][which(hed[,,2:(dis$nlay)] < dis$botm[,,1:(dis$nlay-1)])]
     botm[,,!cbd] <- dis$botm
-    botm[,,which(cbd == 1)] <- botm[,,which(cbd == 1)-1] - thck[,,which(cbd == 1)]
+    if (1 %in% cbd) botm[,,which(cbd == 1)] <- botm[,,which(cbd == 1)-1] - thck[,,which(cbd == 1)]
     dis$botm <- botm
   } 
   dis$top <- rmf_create_array(c(hed[,,1]), dim = c(dis$nrow, dis$ncol))