7d29f4457ac146a5329c73a2f762f27fa037189c
cneyens <cas.neyens@gmail.com>
2019-08-09 15:03:21 +0200

    Merge

--- R/utils.R
+++ R/utils.R
@@ -1026,6 +1026,7 @@ convert_huf_to_mask <- function(...) {
 
 #' Convert a huf object to an rmf_3d_array with the number of numerical layers per hydrogeological unit
 #' 
+<<<<<<< Updated upstream
 #' @param huf huf object
 #' @param dis dis object, corresponding to the huf object
 #' @param bas bas object, corresponding to the huf object
@@ -1059,6 +1060,9 @@ convert_huf_to_nlay <- function(...) {
 #' Convert an \code{ibound} array to lower, upper, left, right, front and back logical arrays indicating presence of a neighbouring active cell
 #' 
 #' @param ibound modflow basic file \code{ibound} array
+=======
+#' @param ibound 3d \code{ibound} array as specified in a MODFLOW BAS object
+>>>>>>> Stashed changes
 #' @return list of lower, upper, left, right, front and back logical 3d arrays
 #' @export
 rmf_convert_ibound_to_neighbours <- function(ibound) {
