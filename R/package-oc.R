ac405c7cac533e867f7fef8f40d173bef4d6a977
cneyens <cas.neyens@gmail.com>
2019-09-04 16:42:42 +0200

    Slightly easier OC API

--- R/package-oc.R
+++ R/package-oc.R
@@ -2,7 +2,7 @@
 #' 
 #' \code{rmf_create_oc} creates an \code{RMODFLOW} oc object.
 #' 
-#' @param dis dis object
+#' @param dis \code{RMODFLOW} dis object
 #' @param ihedfm code for the format in which heads will be printed; defaults to NA (heads are not printed)
 #' @param chedfm code for the format in which heads will be saved; defaults to NA (heads are saved in binary file)
 #' @param ihedun unit number on which heads will be saved; defaults to 666
@@ -17,7 +17,7 @@
 #' @param drawdown_label logical; should labels be included in the unformatted drawdown file?
 #' @param ibound_label logical; should labels be included in the unformatted ibound file?
 #' @param iperoc vector of stress period numbers
-#' @param itsoc vector of time step numbers (within the corresponding stress period)
+#' @param itsoc vector of time step numbers (within the corresponding stress period). A negative number indicates the last time step within the corresponding stress period
 #' @param print_head logical vector or logical matrix of dimensions NLAY x length(iperoc); should heads be printed for the time step corresponding to iperoc and itsoc? If not a matrix, all layers are treated the same.
 #' @param print_drawdown logical vector or logical matrix of dimensions NLAY x length(iperoc); should drawdowns be printed for the time step corresponding to iperoc and itsoc? If not a matrix, all layers are treated the same.
 #' @param print_budget logical vector; should budget be printed for the time step corresponding to iperoc and itsoc?
@@ -37,7 +37,7 @@
 #' @return Object of class oc
 #' @export
 #' @seealso \code{\link{rmf_read_oc}}, \code{\link{rmf_write_oc}} and \url{http://water.usgs.gov/nrp/gwsoftware/modflow2000/MFDOC/index.html?oc.htm}
-rmf_create_oc <- function(dis = create_dis(),
+rmf_create_oc <- function(dis,
                           ihedfm = NA,
                           chedfm = NA,
                           ihedun = 666,
@@ -53,13 +53,13 @@ rmf_create_oc <- function(dis = create_dis(),
                           ibound_label = TRUE,
                           iperoc = rep(1:dis$nper,dis$nstp),
                           itsoc = unlist(apply(data.frame(1,dis$nstp),1,FUN=function(x) x[1]:x[2])),
-                          print_head = rep(FALSE, length(iperoc)),
-                          print_drawdown = rep(FALSE, length(iperoc)),
-                          print_budget = rep(FALSE, length(iperoc)),
-                          save_head = rep(TRUE, length(iperoc)),
-                          save_drawdown = rep(FALSE, length(iperoc)),
-                          save_ibound = rep(FALSE, length(iperoc)),
-                          save_budget = rep(TRUE, length(iperoc)),
+                          print_head = FALSE,
+                          print_drawdown = FALSE,
+                          print_budget = TRUE,
+                          save_head = TRUE,
+                          save_drawdown = FALSE,
+                          save_ibound = FALSE,
+                          save_budget = TRUE,
                           incode = NULL,
                           ihddfl = NULL,
                           ibudfl = NULL,
@@ -93,16 +93,16 @@ rmf_create_oc <- function(dis = create_dis(),
       
       # data set 2
       oc$iperoc <- iperoc
-      oc$itsoc <- itsoc
+      oc$itsoc <- replace(itsoc, which(itsoc < 0), dis$nstp[oc$iperoc[which(itsoc < 0)]])
       
       # data set 3
-      oc$print_head <- print_head
-      oc$print_drawdown <- print_drawdown
-      oc$print_budget <- print_budget
-      oc$save_head <- save_head
-      oc$save_drawdown <- save_drawdown
-      oc$save_ibound <- save_ibound
-      oc$save_budget <- save_budget
+      oc$print_head <- rmfi_ifelse0(length(print_head) == 1, rep(print_head, length(iperoc)), print_head)
+      oc$print_drawdown <- rmfi_ifelse0(length(print_drawdown) == 1, rep(print_drawdown, length(iperoc)), print_drawdown)
+      oc$print_budget <- rmfi_ifelse0(length(print_budget) == 1, rep(print_budget, length(iperoc)), print_budget)
+      oc$save_head <- rmfi_ifelse0(length(save_head) == 1, rep(save_head, length(iperoc)), save_head)
+      oc$save_drawdown <- rmfi_ifelse0(length(save_drawdown) == 1, rep(save_drawdown, length(iperoc)), save_drawdown)
+      oc$save_ibound <- rmfi_ifelse0(length(save_ibound) == 1, rep(save_ibound, length(iperoc)), save_ibound)
+      oc$save_budget <- rmfi_ifelse0(length(save_budget) == 1, rep(save_budget, length(iperoc)), save_budget)
       
 
     } else { # numeric codes
