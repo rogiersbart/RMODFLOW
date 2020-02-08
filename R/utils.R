fd79d8184e5e0811851f2a6282631a1c1b2fe81e
cneyens <cas.neyens@gmail.com>
2020-02-08 13:29:42 +0100

    Removed rmf_read_gms_2d_grid

--- R/utils.R
+++ R/utils.R
@@ -2570,36 +2570,6 @@ rmf_performance.hpr <- function(hpr, hobdry = -888, measures = c('ssq', 'mse', '
   return(perform)
 }
 
-#' Read a GMS 2D grid file
-#' 
-#' \code{read_gms_2d_grid} reads in a GMS 2D grid file and returns it as an \code{\link{RMODFLOW}} gms2dgrid object.
-#' 
-#' @param file filename
-#' @return object of class gms2dgrid
-#' @export
-rmf_read_gms_2d_grid <- function(file = {cat('Please select gms 2d grid file ...\n'); file.choose()}) {
-  grid2d <- list()
-  grid2d.lines <- readr::read_lines(file)
-  #2dgrid.lines <- remove.comments.from.lines(mlt.lines)
-  grid2d.lines <- grid2d.lines[-1]    
-  grid2d$objtype <- as.character(strsplit(grid2d.lines[1],'\"')[[1]][2])
-  grid2d.lines <- grid2d.lines[-1]
-  grid2d.lines <- grid2d.lines[-1]
-  grid2d$nd <- as.numeric(strsplit(grid2d.lines[1],' ')[[1]][3])
-  grid2d.lines <- grid2d.lines[-1]
-  grid2d$nc <- as.numeric(strsplit(grid2d.lines[1],' ')[[1]][3])    
-  grid2d.lines <- grid2d.lines[-1]
-  grid2d$n <- as.character(strsplit(grid2d.lines[1],'\"')[[1]][2])    
-  grid2d.lines <- grid2d.lines[-1]    
-  grid2d.lines <- grid2d.lines[-1] 
-  
-  grid2d$nddata <- as.numeric(grid2d.lines[1:grid2d$nd])
-  grid2d$ncdata <- as.numeric(grid2d.lines[(1+grid2d$nd):(grid2d$nd + grid2d$nc)])              
-  
-  class(grid2d) <- 'gms_2d_grid'
-  return(grid2d)
-}
-
 #' Read a MODFLOW array from a separate file. 
 #'
 #' \code{rmf_read_array} reads a MODFLOW array from a separate file. Binary and ASCII formats are supported
